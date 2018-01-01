431011f95f34f64c34de0902bddb028cfd9dd05e
SmokeyTheBandicoot <miki.gargiulo@live.it>
2018-01-01 13:09:21 +0100

    Aggiungere i file di progetto.

---
+++ CapsBlockNumIconNotifier/About.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class About
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(About))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(536, 276)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = resources.GetString("TextBox1.Text")
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(405, 294)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(143, 35)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'About
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(560, 338)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "About"
+        Me.Text = "About"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As Windows.Forms.TextBox
+    Friend WithEvents Button1 As Windows.Forms.Button
+End Class
---
+++ CapsBlockNumIconNotifier/Module1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Diagnostics
+Imports System.Text
+Imports System.Windows.Forms
+
+
+
+Public Module Module1
+
+    Private mobNotifyIcon As NotifyIcon
+    Private WithEvents mobContextMenu As ContextMenu
+
+    Dim CAPSIcon As NotifyIcon
+    Dim SCRLKIcon As NotifyIcon
+    Dim NUMLKIcon As NotifyIcon
+
+    Private Sub CreateMenu()
+        Try
+            mobContextMenu.MenuItems.Add(New MenuItem("Toggle CAPS", New EventHandler(AddressOf ToggleCAPS)))
+
+            mobContextMenu.MenuItems.Add(New MenuItem("Toggle BlockNum", New EventHandler(AddressOf ToggleBlockNum)))
+
+            mobContextMenu.MenuItems.Add(New MenuItem("Toggle ScreenLock", New EventHandler(AddressOf ToggleScreenLock)))
+
+            mobContextMenu.MenuItems.Add("-")
+
+            mobContextMenu.MenuItems.Add(New MenuItem("About", New EventHandler(AddressOf AboutBox)))
+
+            mobContextMenu.MenuItems.Add(New MenuItem("Exit", New EventHandler(AddressOf ExitApp)))
+
+        Catch obEx As Exception
+            Throw obEx
+        End Try
+    End Sub
+
+    Public Sub ToggleCAPS()
+
+    End Sub
+
+    Public Sub ToggleBlockNUM()
+
+    End Sub
+
+    Public Sub ToggleScreenLock()
+
+    End Sub
+
+    Public Sub AboutBox()
+
+    End Sub
+
+    Public Sub ExitApp()
+
+    End Sub
+
+    Sub Main()
+        CreateMenu()
+
+        With CAPSIcon
+            .Icon =
+        End With
+    End Sub
+
+End Module
